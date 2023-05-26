0ebceb7c9d040576ba8aa6752c02ea6232258e9a
Anthony Ngo <ango.ext@yieldengineering.net>
2023-05-26 08:05:45 -0700

    convert to class. add method to push to todo list

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-const newTodo = document.getElementById('new-todo');
+const newTodo = document.getElementById("new-todo");
 
 // Array of Task Objects
 const todos = [
-	{
-		task: 'Wash the Dishes',
-		dueDate: '5/12/2023',
-		priority: 0,
-		completed: false,
-	},
+  {
+    task: "Wash the Dishes",
+    dueDate: "5/12/2023",
+    priority: 0,
+    completed: false,
+  },
 ];
 
-const newTask = (task, date, priority) => {
-	let taskToAdd = {
-		task,
-		date,
-		priority,
-		completed: false,
-	};
-	todos.push(taskToAdd);
-};
+// const newTask = (task, date, priority) => {
+//   let taskToAdd = {
+//     task,
+//     date,
+//     priority,
+//     completed: false,
+//   };
+//   todos.push(taskToAdd);
+// };
 
-newTask('Dance', '5/15/2222', 3);
+class Task {
+  constructor(task, date, priority) {
+    this.task = task;
+    this.date = date;
+    this.priority = priority;
+    this.completed = false;
+  }
+  pushToList(todoList) {
+    todoList.push(this);
+    todoList.forEach((todo) => {
+      console.log(todo);
+    });
+  }
+}
 
-todos.forEach((todo) => {
-	console.log(todo);
-});
+let task = new Task("Cook", "2/2/2", 3);
+
+task.pushToList(todos);
+
+const tr = document.createElement("tr");
+tr.classList.add("todo");
+
+const tdCheckbox = document.createElement("td");
