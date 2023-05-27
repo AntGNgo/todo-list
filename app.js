38c2d92ca5ee63a5c7633c0169fc50b03a3e4210
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-05-27 15:24:18 -0700

    add function to add new task

--- app.js
+++ app.js
@@ -1,45 +1,60 @@
-const newTodo = document.getElementById("new-todo");
+const newTodo = document.getElementById('new-todo');
 
 // Array of Task Objects
 const todos = [
-  {
-    task: "Wash the Dishes",
-    dueDate: "5/12/2023",
-    priority: 0,
-    completed: false,
-  },
+	{
+		task: 'Wash the Dishes',
+		dueDate: '5/12/2023',
+		priority: 0,
+		completed: false,
+	},
 ];
 
-// const newTask = (task, date, priority) => {
-//   let taskToAdd = {
-//     task,
-//     date,
-//     priority,
-//     completed: false,
-//   };
-//   todos.push(taskToAdd);
-// };
-
 class Task {
-  constructor(task, date, priority) {
-    this.task = task;
-    this.date = date;
-    this.priority = priority;
-    this.completed = false;
-  }
-  pushToList(todoList) {
-    todoList.push(this);
-    todoList.forEach((todo) => {
-      console.log(todo);
-    });
-  }
+	constructor(task, date, priority) {
+		this.task = task;
+		this.date = date;
+		this.priority = priority;
+		this.completed = false;
+	}
+
+	pushToList(todoList) {
+		todoList.push(this);
+		todoList.forEach((todo) => {
+			console.log(todo);
+		});
+	}
 }
 
-let task = new Task("Cook", "2/2/2", 3);
+newTodo.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter') {
+		let task = new Task(e.target.value, '2/2/2', 0);
+		task.pushToList(todos);
+		e.target.value = '';
+		appendTodo(task.task);
+	}
+});
 
-task.pushToList(todos);
+// Append to DOM
+const appendTodo = (task, date, priority) => {
+	const tBodyRef = document.getElementById('tbody');
+	console.log(tBodyRef);
 
-const tr = document.createElement("tr");
-tr.classList.add("todo");
+	const newRow = tBodyRef.insertRow();
 
-const tdCheckbox = document.createElement("td");
+	newRow.classList.add('todo');
+
+	const cellInput = newRow.insertCell();
+	const inputCheckbox = document.createElement('input');
+	inputCheckbox.setAttribute('type', 'checkbox');
+
+	cellInput.appendChild(inputCheckbox);
+
+	const cellTitle = newRow.insertCell();
+	const cellTitlePara = document.createElement('p');
+	cellTitlePara.classList.add('todo-title');
+	cellTitlePara.textContent = task;
+	cellTitle.appendChild(cellTitlePara);
+};
+
+const tdCheckbox = document.createElement('td');
