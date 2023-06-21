cc04e26e439d7aec4e2c04606a8c5074e11c5b32
Anthony Ngo <anthony.g.ngo@gmail.com>
2023-06-20 18:00:13 -0700

    add new todos to todo array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./newProjectBtn";
+import newTask from "./newTask";
+
+const addTodoBtn = document.getElementById("add-todo-btn");
+const todos = [];
+
+addTodoBtn.addEventListener("click", () => {
+  const task = newTask();
+  todos.push(task);
+  console.log(todos);
+});
--- src/newTask.js
+++ src/newTask.js
@@ -1,9 +1,15 @@
-const todoFactory = () => {
+const newTask = () => {
   const name = document.getElementById("new-todo-name");
   const date = document.getElementById("new-todo-date");
   const priority = document.getElementById("new-todo-priority");
   const project = document.getElementById("new-todo-project");
-  return { name, date, priority, project };
+
+  const nameValue = name.value;
+  const dateValue = date.value;
+  const priorityValue = priority.value;
+  const ProjectValue = project.value;
+
+  return { nameValue, dateValue, priorityValue, ProjectValue };
 };
 
-export default todoFactory;
+export default newTask;
