cc7e23de46d190e751bc80ed53a5b4313ec65fb5
Anthony Ngo <ango.ext@yieldengineering.net>
2023-06-22 06:28:46 -0700

    Validate before appending to DOM

--- src/renderTasks.js
+++ src/renderTasks.js
@@ -1,34 +1,45 @@
 const renderTasks = (tasksArray, filter) => {
-	const tasksDOMRef = document.getElementById('tasks');
-	// TODO make option for filter
-	if (!filter) {
-		while (tasksDOMRef.hasChildNodes()) {
-			tasksDOMRef.removeChild(tasksDOMRef.lastChild);
-		}
-		tasksArray.forEach((task) => {
-			console.log(task.nameValue);
-			const div = document.createElement('div');
-			div.classList.add('task');
+  const tasksDOMRef = document.getElementById("tasks");
+  // TODO make option for filter
+  if (!filter) {
+    while (tasksDOMRef.hasChildNodes()) {
+      tasksDOMRef.removeChild(tasksDOMRef.lastChild);
+    }
+    tasksArray.forEach((task) => {
+      if (task.nameValue.trim() !== "") {
+        console.log(task.nameValue);
+        const div = document.createElement("div");
+        div.classList.add("task");
 
-			const taskName = document.createElement('p');
-			taskName.textContent = task.nameValue;
-			div.appendChild(taskName);
+        const taskName = document.createElement("p");
+        taskName.textContent = task.nameValue;
+        div.appendChild(taskName);
 
-			const taskDue = document.createElement('p');
-			taskDue.textContent = task.dateValue;
-			div.appendChild(taskDue);
+        const taskDue = document.createElement("p");
+        taskDue.textContent = task.dateValue;
+        if (task.dateValue.trim() === "") {
+          taskDue.textContent = "-";
+        }
+        div.appendChild(taskDue);
 
-			const taskPriority = document.createElement('p');
-			taskPriority.textContent = task.priorityValue;
-			div.appendChild(taskPriority);
+        const taskPriority = document.createElement("p");
+        taskPriority.textContent = task.priorityValue;
+        if (task.priorityValue.trim() === "") {
+          taskPriority.textContent = "-";
+        }
+        div.appendChild(taskPriority);
 
-			const taskProject = document.createElement('p');
-			taskProject.textContent = task.projectValue;
-			div.appendChild(taskProject);
+        const taskProject = document.createElement("p");
+        taskProject.textContent = task.projectValue;
+        if (task.projectValue.trim() === "") {
+          taskProject.textContent = "-";
+        }
+        div.appendChild(taskProject);
 
-			tasksDOMRef.appendChild(div);
-		});
-	}
+        tasksDOMRef.appendChild(div);
+      }
+    });
+  }
 };
 
 export default renderTasks;
