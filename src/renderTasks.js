b7d24d33ba8c41f99c37ac1d663f6a2963c97677
Anthony Ngo <ango.ext@yieldengineering.net>
2023-06-23 06:43:18 -0700

    add function to filter by project

--- src/renderTasks.js
+++ src/renderTasks.js
@@ -1,42 +1,52 @@
-const renderTasks = (tasksArray, filter) => {
-  const tasksDOMRef = document.getElementById("tasks");
-  // TODO make option for filter
-  if (!filter) {
-    while (tasksDOMRef.hasChildNodes()) {
-      tasksDOMRef.removeChild(tasksDOMRef.lastChild);
+const tasksDOMRef = document.getElementById("tasks");
+const render = (task) => {
+  if (task.nameValue.trim() !== "") {
+    console.log(task.nameValue);
+    const div = document.createElement("div");
+    div.classList.add("task");
+
+    const taskName = document.createElement("p");
+    taskName.textContent = task.nameValue;
+    div.appendChild(taskName);
+
+    const taskDue = document.createElement("p");
+    taskDue.textContent = task.dateValue;
+    if (task.dateValue.trim() === "") {
+      taskDue.textContent = "-";
     }
+    div.appendChild(taskDue);
+
+    const taskPriority = document.createElement("p");
+    taskPriority.textContent = task.priorityValue;
+    if (task.priorityValue.trim() === "") {
+      taskPriority.textContent = "-";
+    }
+    div.appendChild(taskPriority);
+
+    const taskProject = document.createElement("p");
+    taskProject.textContent = task.projectValue;
+    if (task.projectValue.trim() === "") {
+      taskProject.textContent = "-";
+    }
+    div.appendChild(taskProject);
+
+    tasksDOMRef.appendChild(div);
+  }
+};
+const renderTasks = (tasksArray, filter) => {
+  while (tasksDOMRef.hasChildNodes()) {
+    tasksDOMRef.removeChild(tasksDOMRef.lastChild);
+  }
+
+  if (!filter) {
     tasksArray.forEach((task) => {
-      if (task.nameValue.trim() !== "") {
-        console.log(task.nameValue);
-        const div = document.createElement("div");
-        div.classList.add("task");
-
-        const taskName = document.createElement("p");
-        taskName.textContent = task.nameValue;
-        div.appendChild(taskName);
-
-        const taskDue = document.createElement("p");
-        taskDue.textContent = task.dateValue;
-        if (task.dateValue.trim() === "") {
-          taskDue.textContent = "-";
-        }
-        div.appendChild(taskDue);
-
-        const taskPriority = document.createElement("p");
-        taskPriority.textContent = task.priorityValue;
-        if (task.priorityValue.trim() === "") {
-          taskPriority.textContent = "-";
-        }
-        div.appendChild(taskPriority);
-
-        const taskProject = document.createElement("p");
-        taskProject.textContent = task.projectValue;
-        if (task.projectValue.trim() === "") {
-          taskProject.textContent = "-";
-        }
-        div.appendChild(taskProject);
-
-        tasksDOMRef.appendChild(div);
+      console.log(`rendering: ${task}`);
+      render(task);
+    });
+  } else {
+    tasksArray.forEach((task) => {
+      if (task.projectValue === filter) {
+        render(task);
       }
     });
   }
